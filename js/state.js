8892ac23a24684067534c5a1623378ee5cd8d64b
hayatimaghri <hayatimaghri04@gmail.com>
2026-05-04 15:23:34 +0100

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,19 +1,9 @@
 const state = {
   orders: [],
-<<<<<<< HEAD
-<<<<<<< HEAD
   settings: { restaurantName: "Foodiez", contactEmail: "" },
   filter: "all",
-=======
-  filter: 'all',
->>>>>>> 3fe9538 (new file:   db.json)
+
+ (new file:   db.json),
   loading: false,
   error: null
 };
-=======
-  filter: "all",
-  loading: false,
-  error: null,
-  settings: {},
-};
->>>>>>> 2a6db0e (# Please enter the commit message for your changes. Lines starting)
